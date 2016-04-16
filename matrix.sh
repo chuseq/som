ef69a50b96c4483581316bc03110265eecc00adc
Alfredo Hernandez <chuseq@gmail.com>
2016-04-16 10:52:44 -0500

    Agregue no replicon, ciclo while

--- matrix.sh
+++ matrix.sh
@@ -14,37 +14,40 @@
 ######################################################################
 
 # archivo de salida
-MATRIX=../data/matrix2015lbl2
+MATRIX=../data/matrix20160415ok
 # directorio que contiene los archivos .fna.gz
 GENOMES=../genomes/Bacteria
 
 # primero obtener el listado, sacar la primera linea de cada archivo, 
 # ordenarlas por nombre de bacteria, mandarlas a archivo .srt
-echo -n "Ordenando entrada..."
+echo -n "Generando lista..."
 for f in ${GENOMES}/*.fna.gz 
 do
-    zcat $f |grep "^>"|grep "complete genome"|grep -v chromosome | grep -v plasmid |head -1
-done | sort -t '|' -k 5 > ${MATRIX}.srt
+    bn=$(zcat $f |grep "^>"|grep -v chromosome | grep -v plasmid |grep -v replicon|grep "complete genome"|head -1|cut -d' ' -f 2-)
+    if [ -n "$bn" ] 
+    then
+      echo "$f|$bn"
+    fi
+done |sort -t '|' -k 2 > ${MATRIX}.srt
 echo "Hecho."
 
 # luego leer del archivo .srt, obtener el nombre del archivo de la secuencia, 
 # calcularle el vector, mandarlo a matrix con su etiqueta
-echo -n "Generando matriz"
+echo -n "Generando matriz..."
 echo 16 > ${MATRIX}.dat
 echo > ${MATRIX}.idx
 #echo AA AC AG AT CA CC CG CT GA GC GG GT TA TC TG TT
 i=1
-# e.g. fn=NC_019974
-for fn in $(cat ${MATRIX}.srt | cut -d'|' -f4|cut -d'.' -f1)
+while read fl
 do
-    f=${GENOMES}/${fn}.fna.gz
-    bname=$(zcat $f |head -1 |cut -d'|' -f5|sed 's/^ //g;s/\[//g')
-    lbl=$(echo "$bname"| cut -c 1-4)
+    f=$(echo $fl | cut -d'|' -f1)
+    bn=$(echo $fl | cut -d'|' -f 2-|sed 's/^ //g;s/\[//g;s/\]//g')
+    l1=$(echo "$bn"| cut -c 1-4); l2=$(echo "$bn"|cut -d' ' -f 2|cut -c 1-2); lbl=$l1$l2;
     zcat $f | ./vector >> ${MATRIX}.dat
     # usar $i para numerico, $lbl para etiqueta
-    echo "$i" >> ${MATRIX}.dat
-    echo "$i|$lbl|$fn|$bname" >> ${MATRIX}.idx
+    echo "$lbl" >> ${MATRIX}.dat
+    echo "$i|$lbl|$f|$bn" >> ${MATRIX}.idx
     i=$(($i+1))
-    echo -n .
-done
+#    echo -n .
+done < ${MATRIX}.srt
 echo "Hecho."
